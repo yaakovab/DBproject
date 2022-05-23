a2acfb4d076a1c9f869232e0339d6edef5497a4c
cmiletzky <72802298+cmiletzky@users.noreply.github.com>
2022-05-23 23:39:34 +0300

    Add files via upload

---
+++ Index_creation.sql
@@ -0,0 +1,11 @@
+create index date_prod_index
+on car (proddate)
+
+create index date_index
+on reservation (reservationDate)
+
+create index modelId_index
+on car (modelid)
+
+create index CBDate_index
+on client (cbdate)
